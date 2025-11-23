78b2fcdd33e6c8bf179573ae1d5bc6ad74702b03
Elia Boccini <80979014+Elia-bocc@users.noreply.github.com>
2025-11-23 16:03:11 +0000

    Update get_data.php

--- api/get_data.php
+++ api/get_data.php
@@ -1,19 +1,36 @@
 <?php
+// Configura le credenziali del database Aiven
+$dbhost = 'pg-f9ef4a9-pgdunk-2799.e.aivencloud.com';
+$dbport = '13578';
+$dbname = 'defaultdb';
+$dbuser = 'avnadmin';
+$dbpass = 'AVNS_dP-S1FX9jwwx5uKFUFB';
 
-$conn = pg_connect("host=PG_HOST
-                    port=PG_PORT
-                    dbname=DB_NAME
-                    user=DB_USER
-                    password=DB_PASSWORD
-                    sslmode=require");
+// Connessione al database PostgreSQL con SSL
+$conn = pg_connect("host=$dbhost port=$dbport dbname=$dbname user=$dbuser password=$dbpass sslmode=require");
 
-$result = pg_query($conn, "SELECT * FROM poligoni");
+if (!$conn) {
+    die("Connessione al database fallita: " . pg_last_error());
+}
+
+// Esegui una query di esempio per testare la connessione
+$query = 'SELECT * FROM "Green Spaces";';
+$result = pg_query($conn, $query);
+
+if (!$result) {
+    die("Errore nella query: " . pg_last_error());
+}
+
+// Estrai i risultati e stampali
 $data = [];
-
 while ($row = pg_fetch_assoc($result)) {
     $data[] = $row;
 }
 
 header('Content-Type: application/json');
 echo json_encode($data);
+
+// Chiudi la connessione
+pg_close($conn);
 ?>
+
