a559dbf0169addcd1d0a5f83247b20d670f2f284
Elia Boccini <80979014+Elia-bocc@users.noreply.github.com>
2025-11-23 16:42:52 +0000

    Update add_area.php

--- api/add_area.php
+++ api/add_area.php
@@ -1,4 +1,8 @@
 <?php
+header("Access-Control-Allow-Origin: *");  // Permette richieste da qualsiasi dominio
+header("Access-Control-Allow-Methods: GET, POST, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
 header('Content-Type: application/json');
 
 // Aiven details
@@ -42,3 +46,4 @@ if ($result) {
 pg_close($db_conn);
 
 ?>
+
