0fdecb8b425f5f1c2a8b1184aae247860f10da66
Elia Boccini <80979014+Elia-bocc@users.noreply.github.com>
2025-11-23 16:17:11 +0000

    Update get_data.php

--- api/get_data.php
+++ api/get_data.php
@@ -1,37 +1,61 @@
 <?php
-// Configura le credenziali del database Aiven
+
+// I have a postGis database with two tables: Air Pollution and Green Spaces.
+// This script allow the leflet map to take de data from this DB through XAMPP.
+
+
+header('Content-Type: application/json');
+
+// Aiven details
 $dbhost = 'pg-f9ef4a9-pgdunk-2799.e.aivencloud.com';
 $dbport = 13578;
 $dbname = 'defaultdb';
 $dbuser = 'avnadmin';
 $dbpass = 'AVNS_dP-S1FX9jwwx5uKFUFB';
 
-// Connessione al database PostgreSQL con SSL
+// Conncection
 $conn = pg_connect("host=$dbhost port=$dbport dbname=$dbname user=$dbuser password=$dbpass sslmode=require");
 
 if (!$conn) {
     die("Connessione al database fallita: " . pg_last_error());
 }
 
-// Esegui una query di esempio per testare la connessione
-$query = 'SELECT * FROM "Green Spaces";';
-$result = pg_query($conn, $query);
+// Query for "Green Spaces"
+$query_green_spaces = "
+SELECT jsonb_build_object(
+  'type',     'FeatureCollection',
+  'features', jsonb_agg(ST_AsGeoJSON(t.*)::jsonb)
+)
+FROM \"Green Spaces\" t;
+";
 
-if (!$result) {
-    die("Errore nella query: " . pg_last_error());
-}
+// Excecution Green Spaces query
+$result_green_spaces = pg_query($conn, $query_green_spaces);
+$row_green_spaces = pg_fetch_row($result_green_spaces);
 
-// Estrai i risultati e stampali
-$data = [];
-while ($row = pg_fetch_assoc($result)) {
-    $data[] = $row;
-}
+// Query for "Air Pollution"
+$query_air_pollution = "
+SELECT jsonb_build_object(
+  'type',     'FeatureCollection',
+  'features', jsonb_agg(ST_AsGeoJSON(t.*)::jsonb)
+)
+FROM \"Air Pollution\" t;
+";
 
-header('Content-Type: application/json');
+// Excecution Air Pollution query
+$result_air_pollution = pg_query($conn, $query_air_pollution);
+$row_air_pollution = pg_fetch_row($result_air_pollution);
+
+// Combine
+$data = [
+    'green_spaces' => json_decode($row_green_spaces[0]), 
+    'air_pollution' => json_decode($row_air_pollution[0]) 
+];
+
+// Returning json data
 echo json_encode($data);
 
-// Chiudi la connessione
+// Closing connection
 pg_close($conn);
 ?>
-
 
